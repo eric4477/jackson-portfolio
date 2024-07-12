44e65b07e939d0198a56a3b82136f6d2b4987b87
Eric <ericgeorge4477@gmail.com>
2024-07-13 00:59:15 +0300

    Complete SideBar component and integrate React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import SideBar from "./components/SideBar/SideBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="app container-fluid">
+      <div className="row">
+        <SideBar />
+        <div className="col-md-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,65 @@
+import "./SideBar.css";
+import { NavLink } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <div className="sidebar-container p-0 col-md-2">
+      <div className="sidebar px-4 py-5 d-none d-lg-block">
+        <div className="profile-img">
+          <img src="/profile.jpg" alt="profile" />
+        </div>
+        <a href="#" className="name">
+          Jackson Ford
+        </a>
+        <p className="title">
+          <a href="#">UI/UX/DESIGNER</a> IN PHILIPPINES
+        </p>
+        <ul className="sidebar-links">
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              ABOUT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/skills"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              SKILLS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/experience"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              EXPERIENCE
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/work"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              WORK
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
